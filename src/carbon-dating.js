9ecdfd5dfba03eb7397a69bed8e83b374cac3193
happydenis <dao.denis@gmail.com>
2021-03-30 02:54:04 +0300

    feat: add task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,16 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
+module.exports = function dateSample(sampleActivity) {
+  //if (Number.isInteger(sampleActivity)) return false;
+  //console.log(sampleActivity);
+    let num= +(sampleActivity);
+    if (num >0 && typeof(num) == 'number') {
+      let izot =Math.log(MODERN_ACTIVITY/sampleActivity);
+        izot = Math.ceil(izot/(0.693/HALF_LIFE_PERIOD));
+    //  console.log(izot);
+        return izot;
+    }  else return false
+  //throw new CustomError('Not implemented');num == 'string' 
   // remove line with error and write your code here
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(members) {
+let fLetter =[];
+  if (!Array.isArray(members)) return false;
+for (let member of members) {   
+    fLetter.push(member.charAt(0).toUpperCase())   
+}
+return fLetter
+.sort()
+.join('');
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
