6f715655af05db507206fb4f1a2b965af6976233
happydenis <dao.denis@gmail.com>
2021-03-30 03:49:24 +0300

    feat: correct task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,14 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-let fLetter =[];
-  if (!Array.isArray(members)) return false;
-for (let member of members) {   
-    fLetter.push(member.charAt(0).toUpperCase())   
-}
-return fLetter
-.sort()
-.join('');
-  //throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  //console.log(members)
+  let fLetter =[];
+    if (!Array.isArray(members)) return false;
+    for (let member of members) {
+    //console.log(typeof(member))
+      if (typeof(member)== 'string') {
+      fLetter.push(member.trim().charAt(0).toUpperCase());
+      }
+    }
+return fLetter.sort().join('');
 };
+
+
